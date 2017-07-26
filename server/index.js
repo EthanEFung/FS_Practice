2935b83dae926228a67a964c7f5f9f622f11f47f
Ethan Fung <EthanEFung@gmail.com>
2017-07-25 19:28:17 -0700

    Instantiate server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,22 @@
+
+
 // require necessary imports
 
 // have express instance listen in on a PORT
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(parser.urlencoded({ extended: true }));
+
+
+app.listen(PORT, err=> {
+  if(err) {
+    console.log('error on the server');
+  }
+   else {
+     console.log('Successfully conneced to server port: ' + PORT);
+   }
+})
