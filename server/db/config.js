35412a206be4aabb7a2581e796fa9541132040a7
Ethan Fung <EthanEFung@gmail.com>
2017-07-25 19:42:56 -0700

    Connect db

--- server/db/config.js
+++ server/db/config.js
@@ -1,1 +1,9 @@
 // create connection to db
+const Sequelize = require('sequelize');
+
+const db = new Sequelize('postgres://lgdwepfa:O-YaJAWNdy2mZtcuoYzSmhe224YiFCM-@babar.elephantsql.com:5432/lgdwepfa', {
+  dialect: 'postgres',
+  
+});
+
+module.exports = db;
